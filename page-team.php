9d0662845d434fcc8ae730f1e0a90e97e58d58d0
Giuseppe Surace <pinobulini@gmail.com>
2015-04-29 01:50:05 +0200

    team

--- page-team.php
+++ page-team.php
@@ -5,39 +5,50 @@ Template Name: Staff
 get_header(); ?>
 
 	<!-- Main blog .container -->
-	<div class="container">
-	
+    <div class="container">
+	    <h1>I personaggi</h1>
+		<div class="row">
+			<div class="[ col-sm-6 col-md-offset-2 col-md-4 ]">
+				<div class="[ info-card ]">
+					<img style="width: 100%" src="https://scontent-ams.xx.fbcdn.net/hphotos-xtp1/v/l/t1.0-9/11075152_338171299724124_1958237096724414023_n.jpg?oh=c3a3d161ba14c27dc8fe9fb641378c79&oe=55D57D35" />
+					<div class="[ info-card-details ] animate">
+						<div class="[ info-card-header ]">
+							<h1> Giuseppe Surace </h1>
+							<h3> aka Bulini </h3>
+						</div>
+						<div class="[ info-card-detail ]">
+							<!-- Description -->
+							<p>Sviluppatore e ideatore di questo e moltissimi altri portali di prenotazione online di successo. Ha creato questo sito nel 2006 e ne cura lo sviluppo da oltre 9 anni.</p>
+							<div class="social">
+								<a href="https://www.facebook.com/bulini" class="[ social-icon facebook ] animate"><span class="fa fa-facebook"></span></a>
 
-	
-	
-	<!-- Teasers right side wrapper col-->
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" >
-		<div class="row margin-top-10">
-	<h1><?php the_title(); ?></h1>
+								<a href="https://twitter.com/bulini" class="[ social-icon twitter ] animate"><span class="fa fa-twitter"></span></a>
 
-					<hr class="hr-sm">
-					<?php 
-					query_posts('post_type=team');
-					while ( have_posts() ) : the_post(); ?>
-							
-					<!-- ITEM-->
-					<div class="col-md-6">
-					
-							<div class="row text-center">
+								<a href="https://github.com/bulini" class="[ social-icon github ] animate"><span class="fa fa-github-alt"></span></a>
 
-								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12	">
-								<h2><?php the_title(); ?></h2>
-								<?php the_post_thumbnail('thumbnail', array('class' => 'img-responsive alignleft'));?>
-									
-									<?php the_content(); ?>
-								</div>
+								<a href="https://plus.google.com/u/0/115077481218689845626/posts" class="[ social-icon google-plus ] animate"><span class="fa fa-google-plus"></span></a>
 
+								<a href="www.linkedin.com/in/remcintosh/" class="[ social-icon linkedin ] animate"><span class="fa fa-linkedin"></span></a>
+							</div>
 						</div>
 					</div>
-					<!-- /ITEM-->
-					<?php endwhile; ?>
-		
-					
+				</div>
+			</div>
+    		<div class="[ col-sm-6 col-md-4 ]">
+				<div class="[ info-card ]">
+					<img style="width: 100%" src="http://i2.wp.com/www.appartamenti-lefkada.com/wp-content/uploads/2010/10/CIMG1551.jpg?resize=320%2C240" />
+					<div class="[ info-card-details ] animate">
+						<div class="[ info-card-header ]">
+							<h1>Stefano Amici </h1>
+							<h3> aka Travelrent </h3>
+						</div>
+						<div class="[ info-card-detail ]">
+							<!-- Description -->
+							<p>People often say that beauty is in the eye of the beholder, and I say that the most liberating thing about beauty is realizing that you are the beholder. This empowers us to find beauty in places where others have not dared to look including inside ourselves.</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	<!-- /Main blog .container -->
